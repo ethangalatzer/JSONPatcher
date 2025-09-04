4666e277a977c3b03b207fa9312d256acf41757c
ethangalatzer <ethangalatzer@gmail.com>
2025-09-03 19:46:39 -0700

    First commit

---
+++ jsonpatcher/jsonpatcher.py
@@ -0,0 +1,140 @@
+import json
+import click
+
+
+# Checks if the path points to an existent value, a nonexistent value, or an array
+def check_path(json, path):
+    ptr = json
+    for key in path:
+        if type(ptr) == dict and key in ptr:
+            ptr = ptr[key]
+        else:
+            return False
+    if type(ptr) == list:
+        return "arr"
+    return True
+
+
+# Modifies the value at the path into the new_value
+def modify(json, path, new_value):
+    path_validity = check_path(json, path)
+    # Checks that path points to an existent value
+    if not path_validity:
+        click.echo(f"Value at {'.'.join(path)} does not exist")
+        exit()
+    ptr = json
+    for key in path:
+        if key == path[-1]:
+            ptr[key] = new_value
+            break
+        if key not in ptr:
+            ptr[key] = {}
+        ptr = ptr[key]
+    return json
+
+# Adds the value at the path into the new_value (same as modify when not a list)
+def add(json, path, new_value):
+    path_validity = check_path(json, path)
+    # Checks that path points to either a nonexistent value or a list
+    if path_validity and path_validity != "arr":
+        click.echo(f"Value at {'.'.join(path)} already exists")
+        quit()
+    ptr = json
+    for key in path:
+        if key == path[-1]:
+            if key in ptr and type(ptr[key]) == list:
+                ptr[key].append(new_value)
+                break
+            ptr[key] = new_value
+            break
+        if key not in ptr:
+            ptr[key] = {}
+        ptr = ptr[key]
+    return json
+
+# Deletes the value at the path, or all instances of the value when in an array
+def delete(json, path, value=None):
+    path_validity = check_path(json, path)
+    # Checks that path points to an existent value
+    if not path_validity:
+        click.echo(f"Value at {'.'.join(path)} does not exist")
+        exit()
+    ptr = json
+    for key in path:
+        if key == path[-1]:
+            if type(ptr[key]) == list and value != None:
+                while value in ptr[key]:
+                    ptr[key].remove(value)
+                break
+            ptr.pop(key)
+            break
+        ptr = ptr[key]
+    return json
+
+# Gets all needed values from operation
+def operation_values(operation):
+    patch_op = operation["op"]
+    patch_path = operation["path"]
+    patch_path_list = patch_path.split(".")
+    if "value" in operation:
+        patch_value = operation["value"]
+    else:
+        patch_value = None
+    return patch_op, patch_path, patch_path_list, patch_value
+
+# Returns list of patches sorted by change number
+def sort_patches_by_change_number(patches):
+    patch_change_numbers = {}
+    for patch in patches:
+        with open(patch, "r") as json_patch:
+            json_patch_data = json.load(json_patch)
+            patch_change_numbers[json_patch_data["change_number"]] = patch
+    return(dict(sorted(patch_change_numbers.items())))
+
+# Click decorators
+@click.command()
+@click.option("--input", type=str, prompt=True, help="Input JSON file", required=True)
+@click.option("--patch", type=str, prompt=True, help="Patch JSON file(s), write --patch (file) for each", required=True, multiple=True)
+@click.option("--output", type=str, prompt=True, help="Output JSON file", required=True)
+def patch(input, patch, output):
+
+    patch_change_numbers = sort_patches_by_change_number(patch)
+
+    # Reads data file
+    with open(input, "r") as json_input:
+        try:
+            json_data = json.load(json_input)
+        except:
+            click.echo("Could not read input JSON file")
+            exit()
+
+        # Reads patch files in order
+        for patch_file in patch_change_numbers:
+            with open(patch_change_numbers[patch_file], "r") as json_patch:
+
+                json_patch_data = json.load(json_patch)
+                operations = json_patch_data["operations"]
+
+                # Executes operations in order
+                for operation in operations:
+
+                    patch_op, patch_path, patch_path_list, patch_value = operation_values(operation)
+
+                    # Executes operation by case
+                    match patch_op:
+                        case "add":
+                            add(json_data, patch_path_list, patch_value)
+                        case "modify":
+                            modify(json_data, patch_path_list, patch_value)
+                        case "delete":
+                            delete(json_data, patch_path_list, patch_value)
+                        case "_":
+                            # Forces valid op
+                            click.echo("Operation must be add, modify, or delete")
+                            exit()
+
+    # Writes to output file
+    with open(output, "w") as json_output:
+        json.dump(json_data, json_output, indent=4)
+        click.echo("JSON successfully patched")
+    return json_data
---
+++ jsonpatcher/testsuite.py
@@ -0,0 +1,75 @@
+import unittest
+from . import jsonpatcher
+
+
+class PatcherTestSuite(unittest.TestCase):
+    def test_existent_path(self):
+        self.assertEqual(
+            jsonpatcher.check_path({"user": {"name": {"initials": "EG"}, "ID": 1486}},
+                                   ["user", "name", "initials"]),True)
+
+    def test_nonexistent_path(self):
+        self.assertEqual(
+            jsonpatcher.check_path({"user": {"name": {"initials": "EG"}, "ID": 1486}},
+                                   ["user", "name", "initials", "EG", "E"]),False)
+
+    def test_array_path(self):
+        self.assertEqual(
+            jsonpatcher.check_path({"user": {"name": {"initials": "EG"}, "IDs": [1486, 2483, 1932]}},
+                                   ["user", "IDs"]),"arr")
+
+    def test_add_legal_primitive(self):
+        self.assertEqual(
+            jsonpatcher.add({"user": {"name": {"initials": "EG"}, "ID": 1486}}, ["user", "name", "full_name"], "EthanGalatzer"),
+            {"user": {"name": {"initials": "EG", "full_name": "EthanGalatzer"}, "ID": 1486}})
+
+    def test_add_illegal(self):
+        self.assertRaises(SystemExit, jsonpatcher.add, {"user": {"name": {"initials": "EG"}, "ID": 1486}}, ["user", "name"], "EthanGalatzer")
+
+    def test_add_to_array(self):
+        self.assertEqual(
+            jsonpatcher.add({"user": {"name": {"initials": "EG"}, "IDs": [1486, 2483, 1932]}},
+                            ["user", "IDs"], 3294), {"user": {"name": {"initials": "EG"}, "IDs": [1486, 2483, 1932, 3294]}})
+
+    def test_modify_legal_primitive(self):
+        self.assertEqual(
+            jsonpatcher.modify({"user": {"name": {"initials": "EG"}, "ID": 1486}}, ["user", "name"], "EthanGalatzer"),
+            {"user": {"name": "EthanGalatzer", "ID": 1486}})
+
+    def test_modify_illegal(self):
+        self.assertRaises(SystemExit, jsonpatcher.modify, {"user": {"name": {"initials": "EG"}, "ID": 1486}}, ["user", "name", "full_name"], "EthanGalatzer")
+
+    def test_modify_array(self):
+        self.assertEqual(
+            jsonpatcher.modify({"user": {"name": {"initials": "EG"}, "IDs": [1486, 2483, 1932]}},
+                               ["user", "IDs"], 3294),
+            {"user": {"name": {"initials": "EG"}, "IDs": 3294}})
+
+    def test_delete_legal_primitive(self):
+        self.assertEqual(
+            jsonpatcher.delete({"user": {"name": {"initials": "EG"}, "ID": 1486}}, ["user", "name"], "EthanGalatzer"),
+            {"user": {"ID": 1486}})
+
+    def test_delete_illegal(self):
+        self.assertRaises(SystemExit, jsonpatcher.delete, {"user": {"name": {"initials": "EG"}, "ID": 1486}},
+                          ["user", "name", "full_name"], "EthanGalatzer")
+
+    def test_delete_array_value(self):
+        self.assertEqual(
+            jsonpatcher.delete({"user": {"name": {"initials": "EG"}, "IDs": [1486, 2483, 1932, 1486]}},
+                               ["user", "IDs"], 1486),
+            {"user": {"name": {"initials": "EG"}, "IDs": [2483, 1932]}})
+
+    def test_delete_array(self):
+        self.assertEqual(
+            jsonpatcher.delete({"user": {"name": {"initials": "EG"}, "IDs": [1486, 2483, 1932, 1486]}},
+                               ["user", "IDs"]),
+            {"user": {"name": {"initials": "EG"}}})
+
+    def test_operation_values(self):
+        self.assertEqual(jsonpatcher.operation_values({"op": "modify", "path": "user.name", "value": "Alice"}), ("modify", "user.name", ["user", "name"], "Alice"))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
