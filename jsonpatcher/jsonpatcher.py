89b25d5c04a5456baaf955411345e52b6aa3f3dc
ethangalatzer <ethangalatzer@gmail.com>
2025-09-11 00:23:42 -0700

    Better variable names, better file error detection, more expected return type for sort_patches_by_change_number (previously worked, but through a hard-to-see interaction)

--- jsonpatcher/jsonpatcher.py
+++ jsonpatcher/jsonpatcher.py
@@ -3,8 +3,8 @@ import click
 
 
 # Checks if the path points to an existent value, a nonexistent value, or an array
-def check_path(json, path):
-    json_traverser = json
+def check_path(json_data, path):
+    json_traverser = json_data
     for key in path:
         if type(json_traverser) == dict and key in json_traverser:
             json_traverser = json_traverser[key]
@@ -16,29 +16,30 @@ def check_path(json, path):
 
 
 # Modifies the value at the path into the new_value
-def modify(json, path, new_value):
-    path_validity = check_path(json, path)
+def modify(json_data, path, new_value):
+    path_validity = check_path(json_data, path)
     # Checks that path points to an existent value
     if path_validity == "non":
         click.echo(f"Value at {'.'.join(path)} does not exist")
         raise ValueError
-    json_traverser = json
+    json_traverser = json_data
     for key in path:
         if key == path[-1]:
             json_traverser[key] = new_value
             break
 
         json_traverser = json_traverser[key]
-    return json
+    return json_data
+
 
 # Adds the value at the path into the new_value (same as modify when not a list)
-def add(json, path, new_value):
-    path_validity = check_path(json, path)
+def add(json_data, path, new_value):
+    path_validity = check_path(json_data, path)
     # Checks that path points to a nonexistent value
     if path_validity == "key":
         click.echo(f"Value at {'.'.join(path)} already exists")
         raise ValueError
-    json_traverser = json
+    json_traverser = json_data
     for key in path:
         if key == path[-1]:
             if key in json_traverser and type(json_traverser[key]) == list:
@@ -49,26 +50,28 @@ def add(json, path, new_value):
         if key not in json_traverser:
             json_traverser[key] = {}
         json_traverser = json_traverser[key]
-    return json
+    return json_data
+
 
 # Deletes the value at the path, or all instances of the value when in an array
-def delete(json, path, value=None):
-    path_validity = check_path(json, path)
+def delete(json_data, path, value=None):
+    path_validity = check_path(json_data, path)
     # Checks that path points to an existent value
     if path_validity == "non":
         click.echo(f"Value at {'.'.join(path)} does not exist")
         raise ValueError
-    json_traverser = json
+    json_traverser = json_data
     for key in path:
         if key == path[-1]:
-            if type(json_traverser[key]) == list and value != None:
+            if type(json_traverser[key]) == list and value is not None:
                 while value in json_traverser[key]:
                     json_traverser[key].remove(value)
                 break
             json_traverser.pop(key)
             break
         json_traverser = json_traverser[key]
-    return json
+    return json_data
+
 
 # Gets all needed values from operation
 def operation_values(operation):
@@ -81,48 +84,59 @@ def operation_values(operation):
         patch_value = None
     return patch_op, patch_path, patch_path_list, patch_value
 
+
 # Returns list of patches sorted by change number
-def sort_patches_by_change_number(patches):
+def sort_patches_by_change_number(patch_files: tuple[str, ...]):
     patch_change_numbers = {}
-    for patch in patches:
+    for patch_file in patch_files:
         try:
-            with open(patch, "r") as json_patch:
+            with open(patch_file, "r") as json_patch:
                 json_patch_data = json.load(json_patch)
-                patch_change_numbers[json_patch_data["change_number"]] = patch
+                patch_change_numbers[json_patch_data["change_number"]] = patch_file
         except FileNotFoundError:
-            click.echo(f"Patch {patch} does not exist")
+            click.echo(f"Patch {patch_file} does not exist")
             raise FileNotFoundError
         except:
-            click.echo(f"Patch {patch} is invalid")
+            click.echo(f"Patch {patch_file} is invalid")
             raise Exception
-    return dict(sorted(patch_change_numbers.items()))
+    # Sorts by change number, then returns the file names in order as a list
+    return list(dict(sorted(patch_change_numbers.items())).values())
+
 
 # Click decorators
 @click.command()
 @click.option("--input", type=str, prompt=True, help="Input JSON file", required=True)
-@click.option("--patch", type=str, prompt=True, help="Patch JSON file(s), write --patch (file) for each", required=True, multiple=True)
+@click.option("--patch", type=str, prompt=True, help="Patch JSON file(s), write --patch (file) for each", required=True,
+              multiple=True)
 @click.option("--output", type=str, prompt=True, help="Output JSON file", required=True)
-def patch(input, patch, output):
+def patch(input_file: str, patch_files: tuple[str], output_file: str):
     try:
-        patch_change_numbers = sort_patches_by_change_number(patch)
+        sorted_patches = sort_patches_by_change_number(patch_files)
     except FileNotFoundError:
         exit()
-    try:
-        open(input, "r").close()
-    except FileNotFoundError:
-        click.echo(f"Input JSON file does not exist")
+    except:
         exit()
+
+    try:
+        open(input_file, "r").close()
+    except FileNotFoundError:
+        click.echo(f"Input JSON file {input_file} does not exist")
+        exit()
+    except:
+        click.echo(f"Input JSON file {input_file} is invalid")
+        exit()
+
     # Reads data file
-    with open(input, "r") as json_input:
+    with open(input_file, "r") as json_input:
         try:
             json_data = json.load(json_input)
-        except:
+        except json.decoder.JSONDecodeError:
             click.echo("Could not read input JSON file")
             exit()
 
         # Reads patch files in order
-        for patch_file in patch_change_numbers:
-            with open(patch_change_numbers[patch_file], "r") as json_patch:
+        for patch_file in sorted_patches:
+            with open(patch_file, "r") as json_patch:
 
                 json_patch_data = json.load(json_patch)
                 operations = json_patch_data["operations"]
@@ -149,7 +163,12 @@ def patch(input, patch, output):
                         exit()
 
     # Writes to output file
-    with open(output, "w") as json_output:
-        json.dump(json_data, json_output, indent=4)
-        click.echo("JSON successfully patched")
+    try:
+        with open(output_file, "w") as json_output:
+            json.dump(json_data, json_output, indent=4)
+            click.echo("JSON successfully patched")
+    except:
+        click.echo(f"Output JSON file {output_file} could not be written")
+        exit()
+
     return json_data
